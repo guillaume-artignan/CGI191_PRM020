f966dc49b921bba78282a09bc87b7bb35cc1ec16
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2019-09-10 15:49:43 +0200

    Mise à jour de l'activité - Recuperation des avis

--- app/src/main/java/com/example/cgi/AfficheAvisActivity.java
+++ app/src/main/java/com/example/cgi/AfficheAvisActivity.java
@@ -2,6 +2,9 @@ package com.example.cgi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.cgi.model.Avis;
 import com.example.cgi.task.DownloadAvisTask;
@@ -15,6 +18,26 @@ public class AfficheAvisActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affiche_avis);
 
-        new DownloadAvisTask().execute("https://formation-pro.eu:80/avis");
+        new DownloadAvisTask(this).execute("https://formation-pro.eu:80/avis");
+    }
+
+    public void setAvis(Avis[] avis) {
+        ProgressBar progress = findViewById(R.id.progressBar);
+        TextView nbAvis = findViewById(R.id.nb_avis);
+        nbAvis.setText("On a récupéré "+avis.length+" avis");
+        progress.animate()
+                .translationX(500)
+                .alpha(0f)
+                .setDuration(2000);
+
+        nbAvis.setVisibility(View.VISIBLE);
+        nbAvis.setAlpha(0f);
+        nbAvis.animate()
+                .translationX(100)
+                .alpha(1.0f)
+                .setDuration(2000);
+
+
+
     }
 }
--- app/src/main/java/com/example/cgi/task/DownloadAvisTask.java
+++ app/src/main/java/com/example/cgi/task/DownloadAvisTask.java
@@ -3,6 +3,7 @@ package com.example.cgi.task;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.example.cgi.AfficheAvisActivity;
 import com.example.cgi.Utils;
 import com.example.cgi.model.Avis;
 
@@ -10,13 +11,20 @@ import java.io.IOException;
 
 public class DownloadAvisTask extends AsyncTask<String,Integer, Avis[]> {
 
+    private AfficheAvisActivity activity;
+
+    public DownloadAvisTask(AfficheAvisActivity  activity){
+        this.activity = activity;
+    }
+
     @Override
     protected Avis[] doInBackground(String... strings) {
 
         try {
+            Thread.sleep(5000);
             return Utils.getAvis(strings[0]);
 
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             Log.e("Erreur Avis","Erreur dans la récupération"+ e.getMessage());
             return new Avis[]{};
         }
@@ -25,5 +33,6 @@ public class DownloadAvisTask extends AsyncTask<String,Integer, Avis[]> {
     @Override
     protected void onPostExecute(Avis[] avis) {
         Log.i("AVIS",""+avis.length);
+        activity.setAvis(avis);
     }
 }
