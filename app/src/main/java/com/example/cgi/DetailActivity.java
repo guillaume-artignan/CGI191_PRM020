e46b245c442f6e76e8658f212a559ab47fe52d94
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2019-09-10 13:57:12 +0200

    Passage de paramètres 2/2

--- app/src/main/java/com/example/cgi/DetailActivity.java
+++ app/src/main/java/com/example/cgi/DetailActivity.java
@@ -1,8 +1,11 @@
 package com.example.cgi;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.cgi.model.Plat;
@@ -15,8 +18,18 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Intent i = this.getIntent();
         Plat p = (Plat) i.getSerializableExtra("plat");
-        Toast.makeText(this,""+p.getPrix(),Toast.LENGTH_LONG).show();
+       
+        TextView tv_description = findViewById(R.id.description_plat_detail);
+        ImageView image = findViewById(R.id.image_plat_detail);
+        TextView tv_nom = findViewById(R.id.nom_plat_detail);
 
+        tv_nom.setText(p.getNom());
+        tv_description.setText(p.getDescription());
+        image.setImageResource(getId(p.getImage(),"drawable"));
+    }
 
+    private int getId(String nom, String type) {
+        Context context = this;
+        return context.getResources().getIdentifier(nom, type, context.getPackageName());
     }
 }
