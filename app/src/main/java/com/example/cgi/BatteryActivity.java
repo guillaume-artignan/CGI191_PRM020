6b7016ceee88c37628f62146990665d624cd58a0
Guille Pav <guillemette.pavic@yahoo.fr>
2019-09-11 10:40:54 +0200

    Création BatteryActivity + préparation visuelle

--- app/src/main/java/com/example/cgi/BatteryActivity.java
+++ app/src/main/java/com/example/cgi/BatteryActivity.java
@@ -6,16 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class FonctionnalitesActivity extends AppCompatActivity {
+public class BatteryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fonctionnalites);
+        setContentView(R.layout.activity_battery);
     }
 
-    public void afficheBattery (View v){
-        Intent i = new Intent(this,BatteryActivity.class);
-        startActivity(i);
-    }
+
 }
