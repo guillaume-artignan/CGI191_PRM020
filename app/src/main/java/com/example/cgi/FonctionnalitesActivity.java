20170c97866a86bc87029cfc61f8a01d94aef043
dangeol <do.dev@outlook.com>
2019-09-11 12:03:47 +0200

    feat: jouer mp3 sur internet

--- app/src/main/java/com/example/cgi/FonctionnalitesActivity.java
+++ app/src/main/java/com/example/cgi/FonctionnalitesActivity.java
@@ -2,7 +2,9 @@ package com.example.cgi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FonctionnalitesActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class FonctionnalitesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fonctionnalites);
+    }
+
+    public void ouvrirSon(View v){
+        Intent i = new Intent(this,JouerSonActivity.class);
+        startActivity(i);
     }
 }
