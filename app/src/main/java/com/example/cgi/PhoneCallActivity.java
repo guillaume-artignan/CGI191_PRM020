f6387e8d4b9693a671584bab51b7f36a20697eb0
tobie.tourre <tobie.tourre@ipilyon.net>
2019-09-11 10:09:12 +0200

    commit 0

---
+++ app/src/main/java/com/example/cgi/PhoneCallActivity.java
@@ -0,0 +1,14 @@
+package com.example.cgi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_call);
+    }
+}
