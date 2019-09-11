8918581d70707ae883526a92056fd3abb74b6923
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2019-09-11 15:31:12 +0200

    Dernière fonctionnalité Camera

--- app/src/main/java/com/example/cgi/SmsActivity.java
+++ app/src/main/java/com/example/cgi/SmsActivity.java
@@ -4,8 +4,6 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
